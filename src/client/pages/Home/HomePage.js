6eb4edb521752a2815bb0ce68bc0b1d21bb67289
Margob29 <margobiret@yahoo.fr>
2023-03-16 14:47:55 +0100

    presentation frontend

--- src/client/pages/Home/HomePage.js
+++ src/client/pages/Home/HomePage.js
@@ -1,25 +1,52 @@
 import "../../../App.css";
 import cooker from "../../../images/th2.jpeg";
+import RecipeCard from "../../components/RecipeCard";
 
 export default function Home() {
   return (
-    //TODO : components pour les recettes
-    //TODO : superposer le texte et l'image
-    //TODO : responsive
-    <div className="head">
-      <img
-        src={cooker}
-        alt="Cuisinier"
-        width="100%"
-        height="auto"
-        className="sm-none"
-      ></img>
-      <div className="presentation">
-        <p className="textPresentation">Partage tes talents de cuisinier</p>
-        <a className="btnCreation" href="/creation">
-          Créer une recette
-        </a>
+    <div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src={cooker}
+            alt="Cuisinier"
+            width="100%"
+            height="auto"
+            className="d-block w-100"
+          ></img>
+          <div class="carousel-caption d-flex justify-content-center">
+            <div className="presentation">
+              <p className="textPresentation">
+                Partage tes talents de cuisinier
+              </p>
+              <a className="btnCreation" href="/creation">
+                Créer une recette
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
+      <a href="#" className="text-decoration-none">
+        <div className="mt-3">
+          <div className="row ms-3 me-3">
+            <div className="col-xl-3 col-md-6 col-sm-6 p-0">
+              <RecipeCard />
+            </div>
+            <div className="col-xl-3 col-md-6 col-sm-6 p-0">
+              <RecipeCard />
+            </div>
+            <div className="col-xl-3 col-md-6 col-sm-6 p-0">
+              <RecipeCard />
+            </div>
+            <div className="col-xl-3 col-md-6 col-sm-6 p-0">
+              <RecipeCard />
+            </div>
+            <div className="col-xl-3 col-md-6 col-sm-6 p-0">
+              <RecipeCard />
+            </div>
+          </div>
+        </div>
+      </a>
     </div>
   );
 }
